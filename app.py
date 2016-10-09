40cc12f69dedf044e350e9027126fffb567d4ac7
goyyapazham <nalanda99@gmail.com>
2016-10-09 19:25:25 -0400

    session stuff done

--- app.py
+++ app.py
@@ -1,48 +1,39 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, url_for, session
 from utils import account
 
 import hashlib
 
 app = Flask(__name__)
+app.secret_key = "nala_est_tres_cool"
 
 @app.route("/")
+def main():
+    if 'user' in session.keys():
+        return redirect(url_for('home'))
+    return render_template("login.html", message = "")
+
+@app.route("/home/")
 def home():
-    return render_template("main.html")
-
-@app.route("/redirect/", methods = ['POST'])
-def redir():
-    try:
-        if request.form['login'] == "Submit":
-            return redirect("/authenticate/", code=307)
-    except Exception:
-        return redirect("/create/", code=307)
+    return render_template("home.html", username = session['user'])
     
-@app.route("/authenticate/", methods = ['POST'])
+@app.route("/auth/", methods = ['POST'])
 def auth():
-    L = account.get_file()
-    usr = hashlib.sha224(request.form['user']).hexdigest()
-    i = account.is_user(usr, L)
+    sub = request.form['sub']
+    usr = request.form['user']
     pwd = hashlib.sha224(request.form['pass']).hexdigest()
-    resp = ""
-    if i != -1:
-        if account.is_pass(pwd, i, L):
-            resp = "You have logged in successfully."
-        else:
-            resp = "Sorry, the password you submitted was incorrect."
-            
-    else:
-        resp = "Sorry, the username you submitted does not exist."
-    return render_template("response.html", response = resp)
+    if sub == "Login":
+        x = account.login(usr, pwd)
+    if sub == "Register":
+        x = account.register(usr, pwd)
+    if x == 1:
+        session['user'] = usr
+        return redirect(url_for('home'))
+    return render_template("login.html", message = account.get_message(x))
 
-@app.route("/create/", methods = ['POST'])
-def create():
-    L = account.get_file()
-    usr = hashlib.sha224(request.form['user']).hexdigest()
-    pwd = hashlib.sha224(request.form['pass']).hexdigest()
-    if account.is_user(usr, L):
-        return render_template("response.html", response = "Sorry, that username already exists.")
-    account.add_usr_pwd(usr, pwd)
-    return render_template("response.html", response = "Congratulations, your account has successfully been created!")
+@app.route("/logout/", methods = ['POST'])
+def logout():
+    session.pop('user')
+    return redirect(url_for('main'))
 
 if __name__ == '__main__':
     app.debug = True
--- utils/account.py
+++ utils/account.py
@@ -1,3 +1,5 @@
+import hashlib
+
 def get_file():
     f = open("data/accounts.txt", "r")
     L = f.read().splitlines()
@@ -13,7 +15,7 @@ def add_usr_pwd(usr, pwd):
 
 def is_user(usr, L):
     for i in range(len(L)):
-        if L[i][0] == usr:
+        if L[i][0] == usr and usr != "":
             print L[i]
             return i
     return -1
@@ -22,3 +24,39 @@ def is_pass(pwd, i, L):
     if L[i][1] == pwd:
         return True
     return False
+
+def login(usr, pwd):
+    print usr
+    print pwd
+    L = get_file()
+    i = is_user(usr, L)
+    if is_user(usr, L) != -1:
+        if is_pass(pwd, i, L):
+            return 1
+        return 2
+    return 3
+
+def register(usr, pwd):
+    L = get_file()
+    if usr == "":
+        return 4
+    if pwd == hashlib.sha224("").hexdigest():
+        return 5
+    if is_user(usr, L) != -1:
+        return 6
+    add_usr_pwd(usr, pwd)
+    return 7
+
+def get_message(x):
+    if x == 2:
+        return "Sorry, the password you submitted was incorrect."
+    if x == 3:
+        return "Sorry, the username you submitted does not exist."
+    if x == 4:
+        return "Please enter a username."
+    if x == 5:
+        return "Please enter a password."
+    if x == 6:
+        return "Sorry, that username already exists."
+    if x == 7:
+        return "Congratulations, your account has been created!"
